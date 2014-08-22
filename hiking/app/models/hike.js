b4bf104ad84fce3eba5b6c875963ef8863b051c3
Elad Birnboim <eladbirnboim@h-MacBook-Pro-sl-Elad.local>
2014-08-22 16:29:26 +0300

    Added the schema for the hike collection

---
+++ hiking/app/models/hike.js
@@ -0,0 +1,24 @@
+var mongoose 	= require('mongoose');
+
+var hikeSchema = mongoose.Schema({
+
+  name: String,
+  area: String,
+  length: Number,
+  time: String,
+  Difficulty: { type : Number, min : 0 , max :5 },
+  start_coord: {
+    lon: Number,
+    lat: Number
+  },
+  rank_count: Number,
+  avg_overall_rating: { type : Number, min : 0 , max :5 },
+  completed_count: Number,
+  description: String,
+  ranking_pages: [
+    hike_rank_page_id: mongoose.Schema.Types.ObjectId
+  ]
+
+});
+
+module.exports = mongoose.model('Hike', hikeSchema);
