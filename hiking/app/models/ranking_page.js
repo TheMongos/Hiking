4cc1b9573538a00616d7bd9fbf87b5d7d2cab9d6
Elad Birnboim <eladbirnboim@h-MacBook-Pro-sl-Elad.local>
2014-08-22 16:56:47 +0300

    added ranking page schema

---
+++ hiking/app/models/ranking_page.js
@@ -0,0 +1,18 @@
+var mongoose    = require('mongoose');
+
+var rankingPageSchema = mongoose.Schema({
+
+    hike_id: mongoose.Schema.Types.ObjectId,
+    page: Number, 
+    count: Number,
+    comments: [ {
+        rank_id: mongoose.Schema.Types.ObjectId,
+        posted: Date,
+        author: String, 
+        overall_rating: Number,
+        short_description: String
+    } ]
+
+});
+
+module.exports = mongoose.model('RankingPage', rankingPageSchema);
