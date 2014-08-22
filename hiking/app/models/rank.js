d3fd411d02454f1ac70588ebab5edf4068f766bc
David Tzoor <davidtzoor@Davids-MacBook-Air.local>
2014-08-22 16:29:09 +0300

    Added the schema for the rank collection.

---
+++ hiking/app/models/rank.js
@@ -0,0 +1,16 @@
+var mongoose 	= require('mongoose');
+
+var hikeSchema = mongoose.Schema({
+
+	username: 		String, 
+	hike_id: 		mongoose.Schema.Types.ObjectId,
+	hike_name: 		String, 
+	posted: 		{ type: Date, default: Date.now }, 
+	overall_rating: { type: Number, min: 0, max: 5 }, 
+	difficulty: 	{ type: Number, min: 0, max: 5 },
+	cleanliness: 	{ type: Number, min: 0, max: 5 },
+	description: 	String
+
+});
+
+module.exports = mongoose.model('Rank', hikeSchema);
