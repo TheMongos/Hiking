c1a084986316e88336d4cd2280ef6581a567a631
davidtzoor <david.tzoor@gmail.com>
2014-08-23 16:31:30 +0300

    Added username to session after successful log-in.

--- hiking/config/passport.js
+++ hiking/config/passport.js
@@ -55,6 +55,7 @@ module.exports = function(passport) {
 				if (!user.validPassword(password)) 
 					return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
 				
+				req.session.username = username;
 				return done(null, user);	
 
 
